014e9072e92e076a6b0aa534033ac2c236f32c9a
Tufail Sarovar <sarovar143786@gmail.com>
2025-11-06 02:02:04 +0530

    Update BuyActionWindow

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("https://stockpulse-dashboard-ruddy.vercel.app", {
+    axios.post("https://stockpulse-dashboard-ruddy.vercel.app/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
