c87451c5a35fa50a0fb74f96ad602bb3e7b842d2
Tufail Sarovar <sarovar143786@gmail.com>
2025-11-06 00:26:05 +0530

    Add files via upload

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,4 @@
+const {model} = require("mongoose");
+const {PositionsSchema} = require("../schemas/PositionsSchema");
+const PositionsModel = new model("Position", PositionsSchema);
+module.exports = {PositionsModel};
