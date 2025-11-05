365221ebb7038acec05c5d319e5eaa10508f92c6
Tufail Sarovar <sarovar143786@gmail.com>
2025-11-06 00:29:47 +0530

    Delete backend directory

--- backend/model/PositionsModel.js
+++
@@ -1,4 +0,0 @@
-const {model} = require("mongoose");
-const {PositionsSchema} = require("../schemas/PositionsSchema");
-const PositionsModel = new model("Position", PositionsSchema);
-module.exports = {PositionsModel};
