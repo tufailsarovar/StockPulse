8fc6a20ed81e9b380952a53ee27306deff0dd02d
Tufail Sarovar <sarovar143786@gmail.com>
2025-11-06 00:30:25 +0530

    Add files via upload

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,4 @@
+const { model } = require("mongoose");
+const { HoldingsSchema } = require("../schemas/HoldingsSchema");
+const HoldingsModel = new model("holding", HoldingsSchema);
+module.exports = { HoldingsModel };
